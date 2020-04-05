53059b44abffcdd11a15a1cc4c77438c9e3bb342
Dominik Ritter <dritter03@googlemail.com>
2020-04-05 19:50:42 +0200

    Add example how to filter columns

---
+++ examples/column-filter/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import ExtendedTable from "../../src/ExtendedTable.svelte";
+    import ColumnFilter from "./ColumnFilter.svelte";
+    import {default as data} from '../../fixtures/fixtures.json';
+
+    let columnDefinition = [
+        {
+            title: 'Username',
+            propertyPath: 'username',
+        },
+        {
+            title: 'Name',
+            value: (rowData) => `${rowData.title} ${rowData.first_name} ${rowData.last_name}`,
+            propertyPath: 'last_name',
+        },
+        {
+            title: 'Location',
+            propertyPath: 'location.postcode',
+            value: (rowData) => `${rowData.location.postcode} ${rowData.location.state} ${rowData.location.city}`,
+        },
+    ];
+
+    let rows = data.slice(0, 10);
+</script>
+
+<ColumnFilter bind:columns={columnDefinition}></ColumnFilter>
+<ExtendedTable columns={columnDefinition} data={rows}></ExtendedTable>
---
+++ examples/column-filter/README.md
@@ -0,0 +1,7 @@
+# Column Filter
+
+![](../../images/FilterColumns.png)
+
+This demonstrates how you could build a column filter. Have a look at the [ColumnFilter Component](ColumnFilter.svelte).
+
+The ColumnFilters implementation is very basic, you could easily swap it out for a component made out of your favorite Component Library.
