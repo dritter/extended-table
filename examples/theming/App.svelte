253214e34e14ee3d6cfc3e1f5aeb183b79882a4c
Dominik Ritter <dritter03@googlemail.com>
2020-04-08 19:19:58 +0200

    Add example for dark theme

--- examples/theming/App.svelte
+++ examples/theming/App.svelte
@@ -20,14 +20,40 @@
     ];
 
     let rows = data.slice(0, 10);
+
+    let theme = "light";
 </script>
 
+Theme
+<label>
+    <input type="radio" value="light" bind:group={theme} /> Light
+</label>
+<label>
+    <input type="radio" value="dark" bind:group={theme} /> Dark
+</label>
+
 <!-- extra div for scoping -->
-<div>
+<div style="
+        --theme-background-color: {theme === 'light' ? '#ffffff' : '#7f8c8d'};
+        --theme-headline-background-color: {theme === 'light' ? '#ffffff' : '#34495e'};
+        --theme-text-color: {theme === 'light' ? '#34495e' : '#ecf0f1'};
+">
     <ExtendedTable columns={columnDefinition} data={rows}></ExtendedTable>
 </div>
 
 <style>
+    div :global(table) {
+        background-color: var(--theme-background-color);
+        color: var(--theme-text-color);
+    }
+    div :global(table.et th) {
+        background-color: var(--theme-headline-background-color);
+    }
+
+    div :global(tbody .row-even) {
+        background: red;
+    }
+
     div :global(.col-last_name) {
         background-color: goldenrod;
     }
