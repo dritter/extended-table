3296336616fa9f37cf61125eb0c3b94b4e3bc1dc
Dominik Ritter <dritter03@googlemail.com>
2020-04-13 23:46:19 +0200

    Make rows and columns optional in buildTable

--- test/util.js
+++ test/util.js
@@ -1,14 +1,45 @@
 import {fireEvent, render} from "@testing-library/svelte";
 import ExtendedTable from "../src/ExtendedTable.svelte";
 
-export const buildTable = (rows, columns, props) => {
-    props = props || {};
-    !props.sortingFunction ? props.sortingFunction = () => {} : true;
+
+const defaultRows = [
+    { col1: {subprop: 1}, col2: "xabc", unrelated: "test1", col3: "hier ist zzz"},
+    { col1: {subprop: 2}, col2: "test", unrelated: "test2", col3: "hier ist zzzX"},
+    { col1: {subprop: 3}, col2: "zzz", unrelated: "test3", col3: "hier ist zzzA"},
+    { col1: {subprop: 4}, col2: "bla", unrelated: "test4", col3: "hier ist zzzT"},
+];
+
+let defaultColumns = [
+    {
+        title: 'Column1',
+        propertyPath: 'col1.subprop',
+    },
+    {
+        title: 'Column2',
+        propertyPath: 'col2',
+        collapsed: true
+    },
+    {
+        title: 'Column3',
+        propertyPath: 'col3',
+        collapsed: true
+    },
+];
+
+const freshCopy = (obj) => JSON.parse(JSON.stringify(obj));
+
+export const buildTable = (config) => {
+    config = config || {};
+    config.props = config.props || {};
+    !config.props.sortingFunction ? config.props.sortingFunction = () => {} : true;
+
+    let columns = config.columns || freshCopy(defaultColumns);
+    let rows = config.rows || freshCopy(defaultRows);
 
     const { container } = render(ExtendedTable, {
         data: rows,
         columns: columns,
-        ...props
+        ...config.props
     });
 
     const slugEx = new RegExp(/[^a-z0-9]/g);
@@ -43,5 +74,9 @@ export const buildTable = (rows, columns, props) => {
         };
     });
 
-    return container;
+    return {
+        ref: container,
+        columns: columns,
+        rows: rows
+    };
 };
