b090e3c8931a0f85670f3ea0b4962b8c265c222f
Dominik Ritter <dritter03@googlemail.com>
2020-04-13 22:44:55 +0200

    Simplify collapse tests

--- test/collapse.spec.js
+++ test/collapse.spec.js
@@ -1,7 +1,5 @@
-import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/svelte'
-
-import ExtendedTable from '../src/ExtendedTable.svelte'
+import '@testing-library/jest-dom/extend-expect';
+import { buildTable } from './util';
 
 const originalRows = [
     { col1: {subprop: 1}, col2: "xabc", unrelated: "test1", col3: "hier ist zzz"},
@@ -28,74 +26,49 @@ let originalColumnDefinition = [
 ];
 
 
-const getColumn = (nodeList) => {
-    return Array.prototype.map.call(nodeList, (element) => element.textContent.trim());
-};
-
 test('expanding works properly', async () => {
     const rows = JSON.parse(JSON.stringify(originalRows));
     const columnDefinition = JSON.parse(JSON.stringify(originalColumnDefinition));
-    const { container, getByText } = render(ExtendedTable, {
-        data: rows,
-        columns: columnDefinition,
-        sortingFunction: () => {}
-    });
+    buildTable(rows, columnDefinition);
 
-    const column2Headline = container.querySelector('.col-head-col2');
-    expect(column2Headline).toBeInTheDocument();
-    expect(column2Headline.textContent.trim()).toEqual('...');
+    const column2Headline = columnDefinition[1];
+    expect(column2Headline.getHeadline()).toEqual('...');
+    expect(column2Headline.getValues()).toEqual(["...", "...", "...", "..."]);
 
-    const testColumn = container.querySelectorAll('.col-col2');
-    expect(getColumn(testColumn)).toEqual(["...", "...", "...", "..."]);
+    await column2Headline.expand();
 
-    await fireEvent.click(column2Headline.querySelector('div'));
-    const testColumnHeadlineReselected = container.querySelector('.col-head-col2');
-    expect(testColumnHeadlineReselected.textContent.trim()).toEqual('Column2');
-
-    // Select again after resorting
-    const testColumnReselected = container.querySelectorAll('.col-col2');
-    expect(getColumn(testColumnReselected)).toEqual(["xabc", "test", "zzz", "bla"]);
+    expect(column2Headline.getHeadline()).toEqual('Column2');
+    expect(column2Headline.getValues()).toEqual(["xabc", "test", "zzz", "bla"]);
 });
 
 test('expanding all columns works properly', async () => {
     const rows = JSON.parse(JSON.stringify(originalRows));
     const columnDefinition = JSON.parse(JSON.stringify(originalColumnDefinition));
-    const { container, getByText } = render(ExtendedTable, {
-        data: rows,
-        columns: columnDefinition,
-        expandAll: true,
-        sortingFunction: () => {}
-    });
 
-    const column2Headline = container.querySelector('.col-head-col2');
-    expect(column2Headline).toBeInTheDocument();
-    expect(column2Headline.textContent.trim()).toEqual('...');
+    const table = buildTable(rows, columnDefinition, {expandAll: true});
 
-    const testColumn = container.querySelectorAll('.col-col3');
-    expect(getColumn(testColumn)).toEqual(["...", "...", "...", "..."]);
+    const column2Headline = columnDefinition[1];
+    expect(column2Headline.getHeadline()).toEqual('...');
 
-    await fireEvent.click(column2Headline.querySelector('div'));
+    const testColumn = columnDefinition[2];
+    expect(testColumn.getValues()).toEqual(["...", "...", "...", "..."]);
+
+    await column2Headline.expand();
 
     // Select again after resorting
-    const testColumnReselected = container.querySelectorAll('.col-col3');
-    expect(getColumn(testColumnReselected)).toEqual(["hier ist zzz", "hier ist zzzX", "hier ist zzzA", "hier ist zzzT"]);
+    expect(testColumn.getValues()).toEqual(["hier ist zzz", "hier ist zzzX", "hier ist zzzA", "hier ist zzzT"]);
 });
 
 test('expanding does not trigger sorting', async () => {
     const rows = JSON.parse(JSON.stringify(originalRows));
     const columnDefinition = JSON.parse(JSON.stringify(originalColumnDefinition));
-    const { container, getByText } = render(ExtendedTable, {
-        data: rows,
-        columns: columnDefinition,
-        sortingFunction: () => {}
-    });
+    const table = buildTable(rows, columnDefinition);
 
-    const column2Headline = container.querySelector('.col-head-col2');
+    const column2Headline = columnDefinition[1];
 
-    const testColumn = container.querySelectorAll('.col-col1_subprop');
-    expect(getColumn(testColumn)).toEqual(["1", "2", "3", "4"]);
+    const testColumn = columnDefinition[0];
+    expect(testColumn.getValues()).toEqual(["1", "2", "3", "4"]);
 
-    await fireEvent.click(column2Headline.querySelector('div'));
-    const testColumnReselected = container.querySelectorAll('.col-col1_subprop');
-    expect(getColumn(testColumnReselected)).toEqual(["1", "2", "3", "4"]);
+    await column2Headline.expand();
+    expect(testColumn.getValues()).toEqual(["1", "2", "3", "4"]);
 });
