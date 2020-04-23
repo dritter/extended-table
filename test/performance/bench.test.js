9d1e4d7179d8deee2cd8ae031cf3785d2f1b76a1
Dominik Ritter <dritter03@googlemail.com>
2020-04-23 22:46:41 +0200

    Fix performance tests

---
+++ test/performance/bench.test.js
@@ -0,0 +1,38 @@
+import {render} from "@testing-library/svelte";
+import '@testing-library/jest-dom/extend-expect';
+import * as Benchmark from 'benchmark';
+const suite = new Benchmark.Suite('ExtendedTable-Performance');
+
+import {buildTable} from "../util.js";
+import SlotRenderingTest from "../Components/SlotRenderingTest.svelte";
+
+const sortableMods = [{sortable: true}, {sortable: true}, {sortable: true}];
+
+/**
+ * @jest-environment jsdom
+ *
+ * Artificial test, because we run the benchmarks in
+ * jest, so that jest-babel handles all the
+ * compiling of svelte and ESM modules for us.
+ */
+test('test', () => {
+    suite
+        .add('default rendering', () => {
+            buildTable({columnMods: sortableMods});
+        })
+        .add('slot rendering', () => {
+            render(SlotRenderingTest);
+        })
+        .add('sorting', () => {
+            const table = buildTable({columnMods: sortableMods});
+            const column2 = table.columns[1];
+
+            (async () => {
+                await column2.click();
+            })();
+        })
+        .on('cycle', event => {
+            console.log(String(event.target));
+        })
+        .run({ async: true });
+});
